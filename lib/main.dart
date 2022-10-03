336218d5bccecc4ca3f12b4f88df3c129ad6fe8c
jaiadityan <jaiadityan6@gmail.com>
2022-10-03 13:16:40 +0530

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,32 @@ class AgriC extends StatelessWidget{
     return MaterialApp(
       title: "App 1",
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text("Hello"),
+          backgroundColor: Colors.pink,
+          title: const Text("Container"),
+          leading: const Icon(Icons.home),
         ),
-        body: const Center(
-          child: Text("HELLO WORLD"),
-        ),
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: const EdgeInsets.all(20),
+            // padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+            border: Border.all(width: 4,color: Colors.black),
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.orange,offset: Offset(6.0,6.0),
+              )
+            ]
+            ),
+            child: const Text("Hello World"),
+          ),
+        )
       ),
     );
   }
